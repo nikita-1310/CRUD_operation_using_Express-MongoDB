a33f10b38afb9bf5117cfca9918bbc278ee00cdc
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-05-12 22:05:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("./src/db/conn");
 const router = require("./src/routes/UserRouter");
 const routerLogin = require("./src/routes/loginRouter");
 
-const app= express()
+const app= express();
 const port = process.env.PORT || 4000;
 app.use(express.json())  // to use json format data in express
 

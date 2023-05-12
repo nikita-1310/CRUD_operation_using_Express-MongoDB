2793d6220f47bf699435c5f3a790c1e0682b6ac5
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-05-12 22:03:05 +0530

    Update JWT.js

--- src/routes/JWT.js
+++ src/routes/JWT.js
@@ -1,4 +1,4 @@
-const {sign , verify} = require("jsonwebtoken")
+const {sign , verify} = require("jsonwebtoken");
 
 const createTokens= (user)=>{
     const accessToken = sign({username:user.username, id:user.id} , 
